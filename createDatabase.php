861f0ceda6d4a61a2ff696817373388ee98abd62
ryanjones-ljmu <R.Jones4@2021.ljmu.ac.uk>
2023-02-06 15:47:09 +0000

    Made additional functions that create both the User Table and the Upload Table where the users can insert the information regarding the files.

--- createDatabase.php
+++ createDatabase.php
@@ -25,6 +25,37 @@ class DBConnection {
         }
     }
 
+    private function createUserTable ($conn) {
+        $sql = "CREATE TABLE UserDetails (
+        userID int AUTO_INCREMENT,
+        username varchar NOT NULL,
+        passwordHash varchar NOT NULL,
+        pathToUserFiles varchar NOT NULL,
+        PRIMARY KEY (userID))";
+        $this->checkTableCreated($conn, $sql);
+    }
+
+    private function createUploadTable ($conn) {
+        $sql = "CREATE TABLE UploadDetails (
+        uploadID int AUTO_INCREMENT PRIMARY KEY,
+        userID int NOT NULL,
+        fileName varchar NOT NULL,
+        typeOfFileUploaded varchar NOT NULL,
+        PRIMARY KEY (uploadID),
+        FOREIGN KEY (userID) REFERENCES UserDetails(userID)
+        )";
+        $this->checkTableCreated($conn, $sql);
+    }
+
+    function checkTableCreated ($conn, $sql) {
+        if ($conn -> query($sql) === TRUE) {
+            echo "The database table was successfully created";
+        }
+        else {
+            echo "There was an error creating the database table: " . $conn -> error;
+        }
+    }
+
 }
 
 
