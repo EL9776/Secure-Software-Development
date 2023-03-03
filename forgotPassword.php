a130fbc133ee9d41cbaa58410a238f9f3736d66b
ashmoran2002@gmail.com <ashmoran2002@gmail.com>
2023-03-03 19:59:19 +0000

    Forgot password email and database table data entry working

--- forgotPassword.php
+++ forgotPassword.php
@@ -7,18 +7,16 @@ $cssPath="resources/forgotPassword.css";
 $bodyContent=<<<BODY
 
 <div class="forgotPasswordForm">
-<form action="/index.php" method="POST" style="...">
-  <div class="container"> 
+<form action="/resetRequest.php" method="POST">
+  <div class="container">
     <h1>Reset Password</h1>
     <p>An email will be sent to you with instructions on how to reset your password.</p>
     <hr>
     <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
-   
+    <input type="text" placeholder="Enter Email" name="email">
     <div class="clearfix">
-      <button type="reset" name="resetRequest">Send Email to Reset Password</button>
+      <button type="submit" name="resetRequest">Send Email to Reset Password</button>
     </div>
-  
   </div>
 </form>
 </div>
